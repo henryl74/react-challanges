d7d3bc0db1ca7acfdc9e7c71c6ead7307f3c0b33
Henry Leon <henryatusabes@gmail.com>
2023-01-12 20:56:36 +0000

    Complete Handling Events Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import css from './App.module.css';
-// import Sidebar from "./components/Sidebar";
-import NavBarSimple from "./components/NavBarSimple";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarSimple />
-      {/* <Sidebar /> */}
+      {/* <NavBarSimple /> */}
+      <NavBarForm />
+      <Sidebar />
     </div>
   );
 }

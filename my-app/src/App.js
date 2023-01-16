1203f763ac046c4a27e554681a9564545d1e0564
Henry Leon <henryatusabes@gmail.com>
2023-01-16 22:20:37 +0000

    Completed Rendering Lists Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from './App.module.css';
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
---
+++ my-app/src/components/Content.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import { savedPosts } from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
+
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false,
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts
+            })
+        }, 500);
+    }
+
+
+    handleSearchBar = (event) => {
+        let textInput = event.target.value.toLowerCase()
+        let filteredPosts = savedPosts.filter((post) => {
+            return post.name.includes(textInput)
+        })
+        this.setState({
+            posts: filteredPosts 
+        })
+    }
+
+    render() {
+        return (
+            <div className={css.Content}>
+
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput">Search:</label>
+                        <input type="search" id="searchInput" placeholder="by author" onChange={this.handleSearchBar} />
+                        <h4>Posts Found: {this.state.posts.length}</h4>
+                    </form>
+                </div>
+
+                <div className={css.SearchResults}>
+                    {/* Part 1: Creating the map function */}
+
+                    {/* {
+                    savedPosts.map((post)=>{
+                            return <div className={css.SearchItem} key={post.title}>
+                                <p>{post.title}</p>
+                                <p>{post.name}</p>
+                                <img src={post.image} alt="random"/>
+                                <p>{post.description}</p>
+                                </div>
+                        })
+                    } */}
+
+
+                    {/* Part 2: Creating a child component */}
+
+                    {
+                        this.state.isLoaded === true ? <PostItem savedPosts={this.state.posts} /> : <Loader />
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content
